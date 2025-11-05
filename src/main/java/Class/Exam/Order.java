d50a335088eed9c114da7b4d560b6534a102760e
Franklin Esedebe <G00465105@atu.ie>
2025-11-05 16:21:38 +0000

    Set up Fields

--- src/main/java/Class/Exam/Order.java
+++ src/main/java/Class/Exam/Order.java
@@ -1,9 +1,35 @@
 package Class.Exam;
 
 public class Order {
-    String orderID, customerName, productName;
+    String OrderID, CustomerName, ProductName;
+    double Price, TotalPrice;
+    int Quantity;
 
     public String getOrderID() {
+        return OrderID;
+    }
+    public void setOrderID(String orderID) {
+        this.OrderID = OrderID;
+    }
+     public String getCustomerName() {
+         return CustomerName;
+     }
+     public void setCustomerName(String customerName) {
+        this.CustomerName = customerName;
+    }
+    public String getProductName() {
+        return ProductName;
+    }
+    public void setProductName(String productName) {
+        this.ProductName = productName;
+    }
 
+    @Override
+    public String toString() {
+        return "Order{" +
+                "OrderID='" + OrderID + '\'' +
+                ",CustomerName" + CustomerName + '\'' +
+                ",ProductName" + ProductName + '\'' +
+                '}';
     }
 }
